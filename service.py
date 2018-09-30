c35d761b57fa9e79be1e8ce17b65a7fc8ae98828
varghese.kallukalam@gmail.com <varghese.kallukalam@gmail.com>
2018-09-30 15:01:58 +1000

    service functions are working

--- service.py
+++ service.py
@@ -1,11 +1,22 @@
-# #Running unix commands with python script
-#Code is required to stop a service in Linux
-#Wait for two minutes 
-#Check the status of the service:
-        #If running -> end program and ask for human intervention
-        #If not running move onto the next service
-#CONCERN: program needs
 import subprocess
+
+def start_service(service):
+    subprocess.call(["service", service, "start"])
+
+    try:
+        output = subprocess.check_output(["service", "apache2", "status"])
+    except subprocess.CalledProcessError as e:
+        output = e.output
+        output = output.decode('utf-8')
+        output = output.split()
+    else:
+        output = output.decode('utf-8')
+        output = output.split()
+
+    if "(running)" in output:
+        print("service is running")
+    elif "(dead)" in output:
+        print("service is not running")
 
 def stop_service(service):
     subprocess.call(["service", service, "stop"])
@@ -25,4 +36,3 @@ def stop_service(service):
     elif "(dead)" in output:
         print("service is not running")
 
-stop_service("apache2")
---
+++ stop_service.py
@@ -0,0 +1,7 @@
+#Stop services
+
+from service import stop_service
+
+stop_service("apache2")
+
+
